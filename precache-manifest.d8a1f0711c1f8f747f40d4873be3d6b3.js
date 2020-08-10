58b154fa4e98da5e858c2a29c7d21d5272972605
Mustafa <stahf@Mustafas-MacBook-Pro.local>
2020-08-10 08:41:52 +0100

    Updates

---
+++ precache-manifest.d8a1f0711c1f8f747f40d4873be3d6b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1daf7672f5d89466e99fa38fffb6499",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "08965c3fc7c706423838",
+    "url": "/task-manager/static/css/main.9965777e.chunk.css"
+  },
+  {
+    "revision": "ff916d8a682ffd3de5a9",
+    "url": "/task-manager/static/js/2.beea2e11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task-manager/static/js/2.beea2e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08965c3fc7c706423838",
+    "url": "/task-manager/static/js/main.89d57b53.chunk.js"
+  },
+  {
+    "revision": "67bcf0315ead2813dc7a",
+    "url": "/task-manager/static/js/runtime-main.02549d15.js"
+  }
+]);
